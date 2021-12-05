3acd1ef157df56113cd8f7b7ab41c35d8f602d73
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-12-05 09:45:37 +0800

    Add Lab Exercise 11 (Perfect Score)

--- Lab Exercise 11/LabExercise11_Viernes.py
+++ Lab Exercise 11/LabExercise11_Viernes.py
@@ -1,40 +1,73 @@
 def main():
-    calc_meals = 0
+    price_list = [("Silog", 10), ("Apple", 20)]
+    minimum_meal_num = 5
+    budget = 70
 
-    prices1 = [("Silog", 10), ("Apple", 20)]
-    min_meals1 = 5
-    budget1 = 70
-
-    print(count_food_combinations(prices1, min_meals1, budget1, calc_meals))
+    possible_meal_permutation = count_food_combinations(price_list, minimum_meal_num, budget, 0)
+    print(possible_meal_permutation)
 
 
-def count_food_combinations(prices, min_meals, budget, calc_meals):
+def count_food_combinations(price_list, minimum_meal_num, budget, meals_so_far):
+    # Initialize
+    if type(price_list[0]) is not int:
+        price_list.sort(key=lambda meal_price: meal_price[1])
+        min_price = price_list[0][1]
+        price_list.insert(0, min_price)
 
-    if calc_meals == 0:
-        calc_meals = [0, []]
-
-    # Base case/s.
-    if calc_meals[0] == budget:
-        return tuple(calc_meals[1])
-    # Backtrack.
-    elif calc_meals[0] > budget:
-        return None
+        return count_food_combinations(price_list, minimum_meal_num, budget, meals_so_far)
     else:
-        meal_combinations = []
+        # Base cases (if the meal combination/s so far satisfies the
+        # given requirements).
+        if price_list[0] > budget:
+            if meals_so_far >= minimum_meal_num:
+                return True
+            else:
+                return 0
 
-        for price in prices:
-            cm_metadata = [calc_meals[0] + price[1], calc_meals[1] + [price[1]]]
+        possible_meal_combinations = 0
+        for meal_price in price_list[1:]:
+            if budget >= meal_price[1]:
+                temp_meal_count = count_food_combinations(price_list, minimum_meal_num,
+                                                          budget-meal_price[1], meals_so_far + 1)
 
-            mc = count_food_combinations(prices, min_meals, budget, cm_metadata)
+                if temp_meal_count is True:
+                    possible_meal_combinations += 1
+                else:
+                    possible_meal_combinations += temp_meal_count
 
-            if (type(mc) is tuple) and len(mc) >= min_meals:
-                meal_combinations.append(mc)
-
-            if type(mc) is list:
-                meal_combinations.extend(mc)
-
-        return meal_combinations
+        return possible_meal_combinations
 
 
 if __name__ == '__main__':
     main()
+
+
+# def count_food_combinations(price_list, minimum_meal_num, budget, meals_so_far):
+#     # Initialize
+#     if type(price_list[0]) is not int:
+#         price_list.sort(key=lambda meal_price: meal_price[1])
+#         min_price = price_list[0][1]
+#         price_list.insert(0, min_price)
+#
+#         return count_food_combinations(price_list, minimum_meal_num, budget, meals_so_far)
+#     else:
+#         # Base cases (if the meal combination/s so far satisfies the
+#         # given requirements).
+#         if price_list[0] > budget:
+#             if meals_so_far >= minimum_meal_num:
+#                 return True
+#             else:
+#                 return 0
+#
+#         possible_meal_combinations = 0
+#         for meal_price in price_list[1:]:
+#             if budget >= meal_price[1]:
+#                 temp_meal_count = count_food_combinations(price_list, minimum_meal_num,
+#                                                           budget-meal_price[1], meals_so_far + 1)
+#
+#                 if temp_meal_count is True:
+#                     possible_meal_combinations += 1
+#                 else:
+#                     possible_meal_combinations += temp_meal_count
+#
+#         return possible_meal_combinations
